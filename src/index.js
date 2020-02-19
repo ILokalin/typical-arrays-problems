390aee67159341a138331090524d6b635d741157
ILokalin <51790923+ILokalin@users.noreply.github.com>
2020-02-20 02:48:41 +0500

    feat: create solution - min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!Array.isArray(array) || array.length === 0) {
+    return 0;
+  }
+
+  let min = array[0];
+  array.forEach(item => {
+    if (item < min) {
+      min = item;
+    }
+  })
+
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!Array.isArray(array) || array.length === 0) {
+    return 0;
+  }
+
+  let max = array[0];
+  array.forEach(item => {
+    if (item > max) {
+      max = item;
+    }
+  })
+
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!Array.isArray(array) || array.length === 0) {
+    return 0;
+  }
+
+  return array.reduce((prev, item) => prev + item, 0) / array.length;
 }
